6f2f26812abe7d43dd93c8cf8369ffae4c0e076a
KimRoks <kimroks3161@gmail.com>
2026-02-04 20:52:27 +0900

    chore: 의존성 및 생성 파일 업데이트
    - iOS Podfile.lock 업데이트
    - macOS GeneratedPluginRegistrant 업데이트
    - devtools_options.yaml 생성

--- lawding_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lawding_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import package_info_plus
+import shared_preferences_foundation
 import url_launcher_macos
 import webview_flutter_wkwebview
 
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
