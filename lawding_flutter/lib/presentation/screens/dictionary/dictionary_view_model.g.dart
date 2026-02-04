3a76757786f8dd8bb3f26a4706d9188fbd946bef
KimRoks <kimroks3161@gmail.com>
2026-02-04 20:52:09 +0900

    feat: Dictionary 화면 ViewModel 패턴 적용 및 검색 기능 구현
    - DictionaryViewModel 생성하여 비즈니스 로직 분리
    - 카테고리 필터링 및 검색 기능 구현
    - 필터링 로직 중복 제거 (_applyFilters 헬퍼 메서드)
    - SharedPreferences 의존성 추가 (최초 접근 가이드용)
    - 검색 박스 디자인 개선 (magnifying/delete 아이콘)
    - CustomScaffold 적용

---
+++ lawding_flutter/lib/presentation/screens/dictionary/dictionary_view_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dictionary_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dictionaryViewModelHash() =>
+    r'cc0ff5e8f6f695d90d51042d2c50b8b4b3af4f75';
+
+/// See also [DictionaryViewModel].
+@ProviderFor(DictionaryViewModel)
+final dictionaryViewModelProvider =
+    AutoDisposeNotifierProvider<DictionaryViewModel, DictionaryState>.internal(
+      DictionaryViewModel.new,
+      name: r'dictionaryViewModelProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$dictionaryViewModelHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$DictionaryViewModel = AutoDisposeNotifier<DictionaryState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
