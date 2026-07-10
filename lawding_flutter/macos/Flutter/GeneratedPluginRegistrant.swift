0a54843c0b46c8182438c892bc4bacb64a2fc9f2
KimRoks <113083860+KimRoks@users.noreply.github.com>
2026-07-10 18:45:55 +0900

    chore: 코드 포맷팅 및 플랫폼 생성 파일 업데이트

--- lawding_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lawding_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,11 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
+import flutter_secure_storage_macos
 import in_app_review
 import package_info_plus
 import shared_preferences_foundation
@@ -15,9 +17,11 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
